0e225bedff4f20b56f5ddbc68d5c1bddfd8d485a
CodyPluchak <pbkid21@yahoo.com>
2021-07-19 18:08:35 -0400

    added switch case player selection and default variables for scoring

---
+++ scoring.js
@@ -0,0 +1,25 @@
+function calculateScore(player) {
+  let score = 0
+
+  switch (player.position) {
+    case ('QB'):
+      score = calculateQBScore(player.stats)
+      break
+    case ('RB'):
+      score = calculateRBScore(player.stats)
+      break
+    case ('WR'):
+      score = calculateWRScore(player.stats)
+      break
+    case ('TE'):
+      score = calculateTEScore(player.stats)
+      break
+  }
+
+  return score
+}
+
+const td = 6
+const turnover = -3
+const received = 1
+
